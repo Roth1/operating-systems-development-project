f5c69e4bd42452cd3a8185d623537c7c29fde4c6
Roth1 <roth.man@web.de>
2015-12-03 12:45:59 +0100

    Update processus.c

--- processus.c
+++ processus.c
@@ -1,42 +1,59 @@
-#ifndef PROCESSUS_H_
-#define PROCESSUS_H_
-
-#include <cpu.h>
-#include <stdbool.h>
-#include "stdio.h"
-#include <inttypes.h>
-#include "affichage.h"
-#include "segment.h"
-
-#define NB_PROC 2
-
-typedef enum {ELU, ACTIVABLE} etat;
-
-typedef struct t_proc {
-    //signed since error is -1
-    int32_t pid;
-    char nom[10];
-    etat mon_etat;
-    uint32_t regs[5];
-    uint32_t pile[512];
-} t_proc;
+#include "processus.h"
 
 t_proc procs[NB_PROC];
 
-void init_procs(void);
+void init_procs(void) {
+    procs[0].pid = 0;
+    strcpy(procs[0].nom, "idle");
+    procs[0].mon_etat = ELU;
+    procs[1].pid = 1;
+    strcpy(procs[1].nom, "proc1");
+    procs[1].mon_etat = ACTIVABLE;
+    procs[1].regs[1] = (uint32_t)&procs[1].pile[511];
+    procs[1].pile[511] = (uint32_t)proc1;
+}
 
-void ordonnance(void);
+void ordonnance(void) {
+  for(uint32_t i = 0; i < NB_PROC; i++) {
+    if(procs[i].mon_etat == ELU) {
+      uint32_t j = (i + 1) % NB_PROC;
+      procs[i].mon_etat = ACTIVABLE;
+      procs[j].mon_etat = ELU;
+      ctx_sw(procs[i].regs, procs[j].regs);
+    }
+  }
+}
 
-char* mon_nom(void);
+char* mon_nom(void) {
+  for(uint32_t i = 0; i < NB_PROC; i++) {
+    if(procs[i].mon_etat == ELU) {
+      return procs[i].nom;
+    }
+  }
+  return "unknown";
+}
 
-int32_t mon_pid(void);
+int32_t mon_pid(void) {
+  for(uint32_t i = 0; i < NB_PROC; i++) {
+    if(procs[i].mon_etat == ELU) {
+      return procs[i].pid;
+    }
+  }
+  return 0;
+}
 
-void ctx_sw(uint32_t *ancien, uint32_t *nouveau);
+void idle(void) {
+  for(;;) {
+	printf("[%s] pid = %i\n", mon_nom(), mon_pid());
+	for(uint32_t i = 0; i < 100000000; i++);
+	ordonnance();
+  }
+}
 
-// default process, you may idle here
-void idle(void);
-
-// a process we implement
-void proc1(void);
-
-#endif
+void proc1(void) {
+  for(;;) {
+	printf("[%s] pid = %i\n", mon_nom(), mon_pid());
+	for(uint32_t i = 0; i < 100000000; i++);
+	ordonnance();
+  }
+}
